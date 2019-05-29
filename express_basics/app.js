c0d4da393ab0f66a3595ab76c1d4e1f024438aaa
Noah Schairer <nschairer@gmail.com>
2019-05-28 20:48:46 -0400

    serving html

--- express_basics/app.js
+++ express_basics/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 //ORDER MATTERS --- TOP TO BOTTOM WITH MIDDLEWARE
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+//grants read access to path --- when routing in html ignore public and just use /css
+app.use(express.static(path.join(__dirname, 'public')));
+
 //places admin ahead of every route in adminRoutes
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>404 Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000, () => {console.log('server running on port 3000')});
--- express_basics/routes/shop.js
+++ express_basics/routes/shop.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
+
+const rootDir = require('../utils/path');
 
 
 router.get('/', (req, res, next) => {
     console.log('GET SHOP ROUTE')
-    res.send('hello from the shop middleware!');
+    //can't use ../ cause / refers to root folder on OS not project for web
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = router;
